31e90a74ac8b8fed80f0705dc563e39d5e3da73f
Watson Breno <watsonbreno2014@gmail.com>
2024-11-01 14:47:46 -0300

    Button desabilita se email ou senha forem do formato invalido

--- src/components/Button/styles.ts
+++ src/components/Button/styles.ts
@@ -1,17 +1,16 @@
 import styled from 'styled-components';
 
-
 export const ButtonContainer = styled.button`
-    width: 100%;
-    height: 42px;
-    background-color: #81259D;
-    color: #FFF;
+  width: 100%;
+  height: 42px;
+  background-color: ${({ disabled }) => (disabled ? '#ccc' : '#81259D')};
+  color: #FFF;
+  border: 1px solid ${({ disabled }) => (disabled ? '#ccc' : '#81259D')};
+  border-radius: 21px;
+  opacity: ${({ disabled }) => (disabled ? 0.6 : 1)};
+  cursor: ${({ disabled }) => (disabled ? 'not-allowed' : 'pointer')};
 
-    border: 1px solid #81259D;
-    border-radius: 21px;
-
-    &:hover {
-        opacity: 0.6;
-        cursor:pointer;
-    }
-`
+  &:hover {
+      opacity: 0.8;
+  }
+`;
--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -3,7 +3,6 @@ import Button from "../../components/Button";
 import Input from "../../components/Input";
 import { yupResolver } from "@hookform/resolvers/yup";
 import * as yup from "yup";
-
 import { Container, LoginContainer, Column, Spacing, Title } from "./styles";
 import { defaultValues, IFormLogin } from "./types";
 
@@ -12,16 +11,13 @@ const schema = yup
     email: yup.string().email("E-mail inválido").required("Campo obrigatório"),
     password: yup
       .string()
-      .min(6, "No minimo 6 caracteres")
+      .min(6, "No mínimo 6 caracteres")
       .required("Campo obrigatório"),
   })
   .required();
 
 const Login = () => {
-  const {
-    control,
-    formState: { errors, isValid },
-  } = useForm<IFormLogin>({
+  const { control, formState: { errors, isValid } } = useForm<IFormLogin>({
     resolver: yupResolver(schema),
     mode: "onBlur",
     defaultValues,
@@ -49,7 +45,7 @@ const Login = () => {
             errorMessage={errors?.password?.message}
           />
           <Spacing />
-          <Button title="Entrar" />
+          <Button title="Entrar" disabled={!isValid} /> {}
         </Column>
       </LoginContainer>
     </Container>
